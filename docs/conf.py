09263c486d712d0d91b0be5e416878ff37e99a00
EDI-N <45854577+EDI-N@users.noreply.github.com>
2023-09-29 17:50:49 +0300

    fff

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+import os
+import requests
+import shutil
 
 extensions = []
 templates_path = ["_templates"]
@@ -12,6 +15,7 @@ copyright = f"{datetime.now().year} Jeff Forcier"
 exclude_patterns = ["_build"]
 
 html_theme = "alabaster"
+html_static_path = ['_static']
 html_sidebars = {
     "**": [
         "about.html",
@@ -33,3 +37,25 @@ extensions.append("releases")
 releases_github_path = "sphinx-doc/alabaster"
 # Our pre-0.x releases are unstable / mix bugs+features
 releases_unstable_prehistory = True
+
+# source, destination
+video_files = [
+    ("https://overtag.dk/files/enable-pull-request-builders.mp4", "enable-pull-request-builders.mp4")
+]
+
+def download_file(url, local_filename):
+    with requests.get(url, stream=True) as r:
+        with open(local_filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+    return local_filename
+
+rootdir = os.path.dirname(os.path.realpath(__file__))
+videodir = os.path.join(rootdir, "_static", "videos")
+
+if not os.path.exists(videodir):
+    os.makedirs(videodir)
+
+for source, dest in video_files:
+    dest_path = os.path.join(videodir, dest)
+    download_file(source, dest_path)
